a77fbcac80078b846e1050fb82b95cd675fb16af
Adrianti Rusli <adriantirusli@gmail.com>
2023-05-26 18:50:33 +0700

    feat: search list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import debounce from 'lodash.debounce'
 import { useEffect, useState } from 'react'
 import Table from '../features/home/components/Table'
 import { get } from '../helper/api'
@@ -7,6 +8,8 @@ import './styles.scss'
 function Home() {
   const [list, setList] = useState([])
   const [loading, setLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
+  const [filteredData, setFilteredData] = useState([])
 
   const columns = ['UUID', 'Komoditas', 'Provinsi', 'Kota', 'Size', 'Price', 'Tanggal Parsed', 'Timestamp']
 
@@ -25,9 +28,31 @@ function Home() {
     }
   }
 
+  const handleSearch = debounce((value) => {
+    setSearchTerm(value)
+
+    const filtered = list.filter(
+      (item) =>
+        item?.komoditas?.toLowerCase().includes(value.toLowerCase()) ||
+        item?.area_provinsi?.toLowerCase().includes(value.toLowerCase()) ||
+        item?.area_kota?.toLowerCase().includes(value.toLowerCase())
+    )
+
+    setFilteredData(filtered || list)
+  }, 100)
+
   return (
     <div className="container">
-      <Table columns={columns} listPrices={list} loading={loading} />
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Pencarian..."
+          value={searchTerm}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+      <Table columns={columns} listPrices={filteredData} loading={loading} />
     </div>
   )
 }
